a8a0a82df86a9ca5337adf6a126fd732c0bcd390
A Rivero <al.rivero@gmail.com>
2021-09-14 21:57:48 +0200

    ejemplo traducido directamente

--- planetas/N_particulas.py
+++ planetas/N_particulas.py
@@ -1,25 +1,35 @@
-#double h;
+from math import sqrt,sin
+import array 
+def double(n):
+    return array.array('d',[0.0]*n)
+
+h=0.0
 N_par=10
-DEBUG=True
-#double G_Un;
-#double epsilon;
-#double M[N_par];
+DEBUG=False
+EULER=True
+VERLET=not EULER
+G_Un=0.0
+epsilon=0.0
+M=double(N_par)
 
 def main():
-    #double x[N_par],y[N_par],z[N_par],v_x[N_par],v_y[N_par],v_z[N_par];
+    x,y,z,v_x,v_y,v_z = (double(N_par) for _ in range(6) )
     #double p_cdm_x,p_cdm_y,p_cdm_z,s_m,restar_x,restar_y,restar_z;
     #double tiempo;
     #int i,j;
     mesfr:int =1000
+    global G_Un
+    global M
+    global epsilon
+    global h
     G_Un=1000.0
-
     p_cdm_x,p_cdm_y,p_cdm_z,s_m= 0.0,0.0,0.0,0.0
     for i in range(N_par):
         M[i]=1.0+i
         x[i]=(N_par*i + 10)
-        y[i]=68*sqrt((double)i) + 13
+        y[i]=68*sqrt(i) + 13
         z[i]=N_par*sin(6.28*i/N_par)+18
-        v_x[i]=(float)i/(float)(10*N_par)
+        v_x[i]=i/(10*N_par)
         v_y[i]=2*v_x[i]
         v_z[i]=v_x[i]
         p_cdm_x+=M[i]*v_x[i]
@@ -37,7 +47,7 @@ def main():
 
     if DEBUG:
         for i in range(N_par):
-            print("%f %f %f %f %f %f\n",x[i],y[i],z[i],v_x[i],v_y[i],v_z[i])
+            print("%f %f %f %f %f %f" %( x[i],y[i],z[i],v_x[i],v_y[i],v_z[i]))
             
     h=0.0001 #//Paso temporal de la ecuacion diferencial discreta
     epsilon=1.0 #//Para evitar la singularidad en el cero
@@ -52,16 +62,16 @@ def main():
         Escribe_resultados(tiempo,x,y,z,v_x,v_y,v_z)
 
 
-def Evoluciona_dt(double *x, double *y,double *z,double *v_x,double *v_y,double *v_z):
+def Evoluciona_dt(x, y, z, v_x, v_y, v_z):
     #int i;
-    #double Fx[N_par],Fy[N_par],Fz[N_par];
-    #double v_temp_x[N_par],v_temp_y[N_par],v_temp_z[N_par];
+    Fx,Fy,Fz =(double(N_par) for _ in range(3) )
+    v_temp_x,v_temp_y,v_temp_z = (double(N_par) for _ in range(3) )
     if EULER: 
         Calcula_Fuerza(x,y,z,Fx,Fy,Fz);
-        #ifdef DEBUG
+        if DEBUG:
             print("En evoluciona, h=%f\n",h);
             for i in range(N_par):
-                printf("Evol: F[%d]=%f %f %f ,v=%f %f %f \n",i,Fx[i],Fy[i],Fz[i],v_x[i],v_y[i],v_z[i]);
+                print("Evol: F[%d]=%f %f %f ,v=%f %f %f" %(i,Fx[i],Fy[i],Fz[i],v_x[i],v_y[i],v_z[i]));
         for i in range(N_par):
             x[i]+=h*v_x[i];
             y[i]+=h*v_y[i];
@@ -72,64 +82,65 @@ def Evoluciona_dt(double *x, double *y,double *z,double *v_x,double *v_y,double 
             v_z[i]  += (Fz[i]*h/M[i]);
     if VERLET:
         Calcula_Fuerza(x,y,z,Fx,Fy,Fz);
-        #ifdef DEBUG
-            printf("En evoluciona, h=%f\n",h);
+        if DEBUG:
+            printf("En evoluciona, h=%f",h);
             for i in range(N_par):
-                printf("Evol: F[%d]=%f %f %f ,v=%f %f %f \n",i,Fx[i],Fy[i],Fz[i],v_x[i],v_y[i],v_z[i]);
+                print("Evol: F[%d]=%f %f %f ,v=%f %f %f" %( i,Fx[i],Fy[i],Fz[i],v_x[i],v_y[i],v_z[i] ) );
         for i in range(N_par):
             v_temp_x[i]=v_x[i]+0.5*Fx[i]*h/M[i];
             x[i]+=h*v_temp_x[i];
             v_temp_y[i]=v_y[i]+0.5*Fy[i]*h/M[i];
-            y[i]+=h*v_temp_y[i];
+            y[i]+=h*v_temp_y[i]
             v_temp_z[i]=v_z[i]+0.5*Fz[i]*h/M[i];
-            z[i]+=h*v_temp_z[i];
-        Calcula_Fuerza(x,y,z,Fx,Fy,Fz);//Ya hemos cambiado r, esta es la nueva fuerza
+            z[i]+=h*v_temp_z[i]
+        Calcula_Fuerza(x,y,z,Fx,Fy,Fz) #Ya hemos cambiado r, esta es la nueva fuerza
         for i in range(N_par):
-            v_x[i]  = v_temp_x[i]+0.5*Fx[i]*h/M[i];
-            v_y[i]  = v_temp_y[i]+0.5*Fy[i]*h/M[i];
-            v_z[i]  = v_temp_z[i]+0.5*Fz[i]*h/M[i];
+            v_x[i]  = v_temp_x[i]+0.5*Fx[i]*h/M[i]
+            v_y[i]  = v_temp_y[i]+0.5*Fy[i]*h/M[i]
+            v_z[i]  = v_temp_z[i]+0.5*Fz[i]*h/M[i]
 
 
-def Calcula_Fuerza(double *x,double *y,double *z,double *Fx,double *Fy,double *Fz)
+def Calcula_Fuerza(x,y,z,Fx,Fy,Fz):
 
-    double r2,distance;
-    int i,j;
+    #double r2,distance;
+    #int i,j;
     for i in range(N_par):
-        Fx[i]=Fy[i]=Fz[i]=0.0;
+        Fx[i]=Fy[i]=Fz[i]=0.0
         for j in range(N_par):
-            if(i==j)
-                continue;
-            r2=epsilon+(x[i]-x[j])*(x[i]-x[j])+(y[i]-y[j])*(y[i]-y[j])+(z[i]-z[j])*(z[i]-z[j]);
-            distance=sqrt(r2);
-            r2=r2*distance;
-            Fx[i]-=(G_Un*M[i]*M[j]*(x[i]-x[j])/r2);
-            Fy[i]-=(G_Un*M[i]*M[j]*(y[i]-y[j])/r2);
-            Fz[i]-=(G_Un*M[i]*M[j]*(z[i]-z[j])/r2);
-                printf("F[%d]=%f %f %f \n",i,Fx[i],Fy[i],Fz[i]);
+            if(i==j):
+                continue
+            r2=epsilon+(x[i]-x[j])*(x[i]-x[j])+(y[i]-y[j])*(y[i]-y[j])+(z[i]-z[j])*(z[i]-z[j])
+            distance=sqrt(r2)
+            r2=r2*distance
+            Fx[i]-=(G_Un*M[i]*M[j]*(x[i]-x[j])/r2)
+            Fy[i]-=(G_Un*M[i]*M[j]*(y[i]-y[j])/r2)
+            Fz[i]-=(G_Un*M[i]*M[j]*(z[i]-z[j])/r2)
+            if DEBUG:
+                print("F[%d]=%f %f %f" % (i,Fx[i],Fy[i],Fz[i]) )
 
 
 
-def Escribe_resultados(double time,double *x,double *y, double *z,double *v_x,double *v_y,double *v_z)
+def Escribe_resultados(time, x, y, z, v_x, v_y, v_z):
 
-    double r2,distancia;
-    double Energia_c,Energia_p,Energia;
-    int i,j;
+    #double r2,distancia;
+    #double Energia_c,Energia_p,Energia;
+    #int i,j;
 
     Energia_p,Energia_c= 0.0, 0.0
     for i in range(N_par):
         for j in range(N_par):
-            if(j==i)
-                continue;
+            if(j==i):
+                continue
             r2=epsilon+(x[i]-x[j])*(x[i]-x[j])+ (y[i]-y[j])*(y[i]-y[j])+(z[i]-z[j])*(z[i]-z[j]);
             distancia=sqrt(r2);
             Energia_p-=(G_Un*M[i]*M[j]/distancia/2.0); 
                 #/* Cuento todo dos veces !!! por eso /2*/
         Energia_c+=0.5*M[i]*(v_x[i]*v_x[i]+v_y[i]*v_y[i]+v_z[i]*v_z[i]);
-        #ifdef DEBUG
-            printf("i=%3d,r=(%-10.3lf,%-10.3lf,%-10.3lf),v=(%-10.3lf,%-10.3lf,%-10.3lf),Ec=%-10.3lf,Ep=%-10.3lf\n",
-                   i,x[i],y[i],z[i],v_x[i],v_y[i],v_z[i],Energia_c,Energia_p);
-    Energia=Energia_c+Energia_p;
-    printf(" %f %f %f %lf %f %f %f %f %f %f \n",time, Energia_c,Energia_p, Energia,x[0],y[0],z[0],x[1],y[1],z[1]);
+        if DEBUG:
+            print("i=%3d,r=(%-10.3lf,%-10.3lf,%-10.3lf),v=(%-10.3lf,%-10.3lf,%-10.3lf),Ec=%-10.3lf,Ep=%-10.3lf\n",
+                   i,x[i],y[i],z[i],v_x[i],v_y[i],v_z[i],Energia_c,Energia_p)
+    Energia=Energia_c+Energia_p
+    print(" %f %f %f %lf %f %f %f %f %f %f" %(time, Energia_c,Energia_p, Energia,x[0],y[0],z[0],x[1],y[1],z[1] ));
     #//getchar();
 
 main()
